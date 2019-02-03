26bd9efa67a65893795139ff657a30fa31fcd05c
zhanghongjun <116004974@qq.com>
2019-02-03 21:15:45 +0800

    跨域

--- public/index.php
+++ public/index.php
@@ -11,6 +11,10 @@
 
 // 绑定前台模块
 define('BIND_MODULE', 'api');
+// 设置跨域请求头
+header("Access-Control-Allow-Origin: *");
 
+// 如果需要自定义请求头时，直接把请求头添加到下面的参数里面即可
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 // 加载公共引导文件
 require './public.php';
